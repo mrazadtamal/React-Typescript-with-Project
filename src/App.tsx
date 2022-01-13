7528f2243281008fe46a8a42fbed3b47c907f16a
mrazadtamal <mrazadt@gmail.com>
2022-01-13 23:02:31 +0600

    todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputFile from "./components/InputFile";
+import TodoList from "./components/TodoList";
 import { Todo } from "./model";
 
 const App: React.FC = () => {
@@ -23,9 +24,7 @@ const App: React.FC = () => {
     <div className="App">
       <div className="header">AZAD TODO</div>
       <InputFile todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      {todos.map((t) => (
-        <li>{t.todo}</li>
-      ))}
+      <TodoList />
     </div>
   );
 };
